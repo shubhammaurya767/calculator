ea52c1784c68a26a4c69af8a9b65fb0912b6ae1a
shubhammaurya767 <75557570+shubhammaurya767@users.noreply.github.com>
2022-08-06 01:11:18 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
-var buttons = document.getElementsByClassName("button");
-var display = document.getElementById("display");
-
-var operand1 = 0;
-var operand2 = null;
-var operator = null;
+let buttons = document.getElementsByClassName("button");
+let display = document.getElementById("display");
+let prevdisplay ="";
+let operand1 = 0;
+let operand2 = null;
+let operator = null;
 
 function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
 }
 
-for (var i = 0; i < buttons.length; i++) {
+for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
 
-        var value = this.getAttribute('data-value');
-        var text = display.textContent.trim();
+        let value = this.getAttribute('data-value');
+        let text = display.textContent.trim();
 
         if (isOperator(value)) {
             operator = value;
@@ -31,9 +31,9 @@ for (var i = 0; i < buttons.length; i++) {
         else if (value == ".") {
             if (!text.includes('.')) {
                 if(text.length)
-                display.textContent = text + '.';
+                    display.textContent = text + '.';
                 else
-                display.textContent = "0.";
+                    display.textContent = "0.";
             }
         } 
         else if (value == "%") {
@@ -43,7 +43,7 @@ for (var i = 0; i < buttons.length; i++) {
         } 
         else if (value == "=") {
             operand2 = parseFloat(text);
-            var result = eval(operand1 + ' ' + operator + ' ' + operand2);
+            let result = eval(operand1 + ' ' + operator + ' ' + operand2);
             if (result) {
                 display.textContent = result;
                 operand1 = result;
