8e99e22a28bde4137d9c2e396ec977afba69c64d
shubhammaurya767 <75557570+shubhammaurya767@users.noreply.github.com>
2022-08-06 02:30:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
 let buttons = document.getElementsByClassName("button");
 let display = document.getElementById("display");
-let prevdisplay ="";
-let operand1 = 0;
-let operand2 = null;
+let operand1 = null;
 let operator = null;
+let currop = null;
 
 function isOperator(value) {
     return value == "+" || value == "-" || value == "*" || value == "/";
@@ -13,45 +12,73 @@ for (let i = 0; i < buttons.length; i++) {
     buttons[i].addEventListener('click', function () {
 
         let value = this.getAttribute('data-value');
-        let text = display.textContent.trim();
-
         if (isOperator(value)) {
             operator = value;
-            operand1 = parseFloat(text);
-            display.textContent = "";
+            if(operand1==null)
+                operand1=currop;
+            display.textContent =operand1+" "+operator+" ";
+            currop=null;
+            console.log(operator);
         } 
-        else if (value == "ac") {
-            display.textContent = "";
+        else if(value == "c")
+        {
+            currop=null;
+            operand1=null;
+            operator=null;
+            display.textContent="";
+        }
+        else if (value == "ce") {
+            currop=null;
+            if(operand1!=null)
+                display.textContent = operand1+" "+operator+" ";
+            else
+                display.textContent = "";
         } 
         else if (value == "sign") {
-            operand1 = parseFloat(text);
-            operand1 = -1 * operand1;
-            display.textContent = operand1;
+            if(currop!=null)
+            {
+                currop = -1 * currop;
+            if(operand1!=null)
+            {
+                display.textContent = operand1 + " "+ operator + " "+ currop;
+            }
+            else {
+                display.textContent = currop;
+            } 
+        }  
         } 
         else if (value == ".") {
-            if (!text.includes('.')) {
-                if(text.length)
-                    display.textContent = text + '.';
-                else
-                    display.textContent = "0.";
-            }
+            if(currop==null) currop="0.";
+            else if (!currop.includes('.') && currop.length)
+                    currop += '.';
+                if(operand1!=null)
+                    display.textContent = operand1 + " " + operator + " "+ currop;
+                else display.textContent = currop;
+            
         } 
         else if (value == "%") {
-            operand1 = parseFloat(text);
-            operand1 = operand1 / 100;
-            display.textContent = operand1;
+            if(currop!=null) currop=currop/100;
+            if(operand1!=null)
+                    display.textContent = operand1 + " " + operator + " "+ currop;
+            else display.textContent = currop;
         } 
         else if (value == "=") {
-            operand2 = parseFloat(text);
-            let result = eval(operand1 + ' ' + operator + ' ' + operand2);
-            if (result) {
-                display.textContent = result;
-                operand1 = result;
-                operand2 = null;
-                operator = null;
+            let result=-1;
+            if(operand1!=null && operator!=null && currop!=null)
+            {
+                result = eval(operand1 + ' ' + operator + ' ' + currop);
+                display.textContent=result;
+                operand1=result;
+                operator=null;
+                currop=null;
             }
-        } else {
-            display.textContent += value;
+        } 
+        else {
+            if(currop==null) currop=value;
+            else
+                currop+=value;
+            console.log(currop);
+            display.textContent+=value;
         }
     });
 }
